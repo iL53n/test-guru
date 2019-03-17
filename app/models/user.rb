fbcb1761d24d4bfa4fa574de21c4e4a36920134b
iL53n <ilsegribko@gmail.com>
2019-03-17 16:04:51 +0300

    add_user_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def show_level_tests(level)
+    Test.where(level: level)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(login: 'User_1')
+User.create(login: 'User_2')
+User.create(login: 'User_3')
+
+Category.create(title: 'web development')
+Category.create(title: 'software development')
+Category.create(title: 'game development')
+
+Test.create(title: 'Ruby', level: 2, category_id: 1)
+Test.create(title: 'Rails', level: 3, category_id: 1)
+Test.create(title: 'Swift', level: 1, category_id: 2)
+
+Question.create(body: 'Question1?', test_id: 1)
+Question.create(body: 'Question2?', test_id: 2)
+Question.create(body: 'Question3?', test_id: 3)
+
+Answer.create(body: 'yes', correct: true, question_id: 1)
+Answer.create(body: 'no', question_id: 1)
+Answer.create(body: 'maybe', question_id: 1)
