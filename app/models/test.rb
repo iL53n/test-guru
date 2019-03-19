b16b4e54f72edb9a6b2fc9012bea4630e2a6b862
iL53n <ilsegribko@gmail.com>
2019-03-19 20:54:00 +0300

    add_scope_1

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,11 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: 'User', optional: true
 
-  def self.tests_by_category(category)
-    Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
-  end
+  # def self.tests_by_category(category)
+  #   Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
+  # end
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY)}
 end
