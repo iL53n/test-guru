9497d03ddbc224d1a43a70803315cae43c54b9ef
iL53n <ilsegribko@gmail.com>
2019-03-19 11:17:54 +0300

    to_check_1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, class_name: 'User', optional: true
 
   def self.tests_by_category(category)
     Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
