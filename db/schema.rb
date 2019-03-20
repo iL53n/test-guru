bb4cdcc84e5cda12ca22a28cd7b92c1e7d12820f
iL53n <ilsegribko@gmail.com>
2019-03-20 22:32:06 +0300

    add_title_level_unique_BD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_071213) do
+ActiveRecord::Schema.define(version: 2019_03_20_192909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_19_071213) do
     t.datetime "updated_at", null: false
     t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
