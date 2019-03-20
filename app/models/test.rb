637103c9e2ccc2bdb1f61b766727ad2fa68b601f
iL53n <ilsegribko@gmail.com>
2019-03-20 23:09:32 +0300

    scopes_instead_methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -13,11 +13,10 @@ class Test < ApplicationRecord
   # def self.tests_by_category(category)
   #   Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
   # end
+  scope :tests_by_category, -> (category) { Test.joins(:category).where(categories: { title: category }) }
+  scope :titles_by_category, -> (category) { tests_by_category(category).order(title: :desc).pluck(:title) } # <--
 
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY)}
-
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ApplicationRecord
   #   Test.where(level: level).joins('INNER JOIN user_tests ON tests.id = user_tests.test_id').where(user_tests: {user_id: self.id})
   # end
 
+  scope :tests_by_level, -> (level) { tests.where(level: level) }
+
 end
 
