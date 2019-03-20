904e10dceb27278f2adbfaa368d9863c688365a5
iL53n <ilsegribko@gmail.com>
2019-03-20 21:59:13 +0300

    add_model_validates

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,11 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: 'User', optional: true
 
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :level }
+  validates :level, numericality: { greater_than: 0,
+                                    only_integer: true } # я думаю пригодится, чтобы могло быть только целочисленным
+
   # def self.tests_by_category(category)
   #   Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
   # end
@@ -12,4 +17,7 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY)}
+
+
+
 end
