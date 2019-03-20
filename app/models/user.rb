e079f0ceb6384b3e30121bbf8786aef9fc68ff0e
iL53n <ilsegribko@gmail.com>
2019-03-20 22:35:56 +0300

    add_email_to_users_and_validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  validates :login, presence: true
+  validates :email, presence: true
 
   # def tests_by_level(level)
   #   Test.where(level: level).joins('INNER JOIN user_tests ON tests.id = user_tests.test_id').where(user_tests: {user_id: self.id})
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_192909) do
+ActiveRecord::Schema.define(version: 2019_03_20_193359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_192909) do
     t.string "login", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
