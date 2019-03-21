2db33b9395055920990fe5fbfb5b51139cf6d906
iL53n <ilsegribko@gmail.com>
2019-03-21 22:09:51 +0300

    correct_1

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,6 @@
 class Question < ApplicationRecord
-
-  ANSWERS = (1..4).freeze
-
   belongs_to :test
   has_many :answers
 
   validates :body, presence: true
-  validate :validate_amt_answers
-
-  private
-
-  def validate_amt_answers
-    errors.add(:answers) if answers.size != ANSWERS
-  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -10,13 +10,12 @@ class Test < ApplicationRecord
   validates :level, numericality: { greater_than: 0,
                                     only_integer: true } # я думаю пригодится, чтобы могло быть только целочисленным
 
-  # def self.tests_by_category(category)
-  #   Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order('tests.title DESC').pluck('tests.title')
-  # end
-  scope :tests_by_category, -> (category) { Test.joins(:category).where(categories: { title: category }) }
-  scope :titles_by_category, -> (category) { tests_by_category(category).order(title: :desc).pluck(:title) } # <--
-
+  scope :tests_by_category, -> (category) { joins(:category).where(categories: { title: category }) }
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY)}
+
+  def self.titles_by_category(category)
+    tests_by_category(category).order(title: :desc).pluck(:title)
+  end
 end
