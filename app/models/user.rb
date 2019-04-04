744ebb2abd0bd1f56fd310340b3007f9cfd7fb0b
iL53n <ilsegribko@gmail.com>
2019-04-04 21:06:14 +0300

    add_signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
 
   has_many :test_passages
@@ -13,6 +15,16 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest
+    Digest::SHA1.hexdigest(string) # + salt
   end
 
 end
