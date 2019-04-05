e07012618f75fa47a3b0eee3fa8e0fe72e720304
iL53n <ilsegribko@gmail.com>
2019-04-05 17:37:20 +0300

    del_user/auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   # has_many :authored_tests, class_name: 'Test', foreign_key: user_id
+
   has_secure_password
 
   scope :tests_by_level, -> (level) { tests.where(level: level) }
