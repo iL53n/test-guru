9cd4cd08d17619893cc3528b5ded113db056ed6e
iL53n <ilsegribko@gmail.com>
2019-04-09 22:05:07 +0300

    add_devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages
   has_many :tests, through: :test_passages
-  # has_many :authored_tests, class_name: 'Test', foreign_key: user_id
-
-  has_secure_password
 
   validates :login, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
