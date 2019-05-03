b683b2c9856b30f14a584a8affbcd79658aef79e
iL53n <ilsegribko@gmail.com>
2019-05-03 20:45:26 +0300

    add_feedback_migration

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,5 @@
 class Feedback < ApplicationRecord
-  belongs_to :author, class_name: 'User', optional: true
+  belongs_to :user, optional: true
 
   validates :title, presence: true
   validates :messages, presence: true
